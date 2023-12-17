0bceef7438233a327974bb10140d9e92c792110e
enesctnn <enespcetin@gmail.com>
2023-12-18 02:53:33 +0300

    UPDATE

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,16 +1,14 @@
 function CartItem({ item, onAdd, onUpdate }) {
-  const className = 'rounded-full bg-amber-950 text-white text-3xl w-6 h-6 ';
+  const className =
+    'rounded-full bg-amber-950 text-white text-3xl w-8 h-8 hover:bg-amber-900 active:bg-amber-800 active:scale-95 duration-100';
 
   return (
-    <li
-      className="flex flex-row justify-between px-4 whitespace-nowrap text-center"
-
-    >
+    <li className="flex flex-row justify-between px-4 whitespace-nowrap text-center">
       <div>
         {item.name} x {item.quantity}
       </div>
       <span className="w-full">${item.price}</span>
-      <menu className="flex flex-row gap-1">
+      <menu className="flex flex-row gap-2">
         <button className={className} onClick={() => onUpdate(item.id, -1)}>
           -
         </button>
