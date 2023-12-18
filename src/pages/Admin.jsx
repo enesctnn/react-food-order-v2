ea69b4f6abdd643c5c8c68a37b30a1d2b13add70
Muhammet Enes Çetin <enespcetin@gmail.com>
2023-12-18 17:14:56 +0300

    admin page

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,4 @@
+function AdminPage() {
+  return 
+}
+export default AdminPage;
--- src/ui/user-data.js
+++ src/ui/user-data.js
@@ -1,3 +1,12 @@
 export default function getUserData() {
   return JSON.parse(localStorage.getItem('USER_DATA')) || null;
 }
+
+export function getIsAdmin() {
+  const { role } = JSON.parse(localStorage.getItem('USER_DATA'));
+
+  if (role === 'ROLE_ADMIN') {
+    return true;
+  }
+  return false;
+}
